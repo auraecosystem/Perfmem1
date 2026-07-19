a24aa20009d1fafdc9076c03f955b8b5ba9f96fa
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-19 22:42:14 +0100

    Update bash.zsh

--- bash.zsh
+++ bash.zsh
@@ -8,3 +8,8 @@ eval 'set +o history' 2>/dev/null || setopt HIST_IGNORE_SPACE 2>/dev/null
 kernel.googlesource.com,FALSE,/,TRUE,2147483647,o,git-kubulee.KL.gmail.com=1//
 __END__
 eval 'set -o history' 2>/dev/null || unsetopt HIST_IGNORE_SPACE 2>/dev/null
+# Generates the modern autoconf macro engine macros
+autoreconf --install
+
+# Executes your processed script to generate a native Makefile
+./configure

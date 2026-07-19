c8892ba58f55abf276f6940cbd8883821f319121
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-19 18:46:43 +0100

    Create bash.zsh

---
+++ bash.zsh
@@ -0,0 +1,10 @@
+eval 'set +o history' 2>/dev/null || setopt HIST_IGNORE_SPACE 2>/dev/null
+ touch ~/.gitcookies
+ chmod 0600 ~/.gitcookies
+
+ git config --global http.cookiefile ~/.gitcookies
+
+ tr , \\t <<\__END__ >>~/.gitcookies
+kernel.googlesource.com,FALSE,/,TRUE,2147483647,o,git-kubulee.KL.gmail.com=1//
+__END__
+eval 'set -o history' 2>/dev/null || unsetopt HIST_IGNORE_SPACE 2>/dev/null
